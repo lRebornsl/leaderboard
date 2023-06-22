da69be4bc9054e3e0470e16c803459f468123e48
lRebornsl <anthony_vasquez98@hotmail.com>
2023-06-22 13:14:03 -0500

    Modify SCSS and JS files

--- src/modules/crudAPI.js
+++ src/modules/crudAPI.js
@@ -1,21 +1,16 @@
 const axios = require('axios').default;
 
 const id = '22rcGHIbXNHC5TKZCLSM';
-
-export const newGame = async () => {
-  await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-    name: 'Leader Game',
-  });
-};
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
 
 export const postScore = (object) => {
-  axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`, {
+  axios.post(url, {
     user: object.user,
     score: object.score,
   });
 };
 
 export const getScore = async () => {
-  const response = await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`);
+  const response = await axios.get(url);
   return response.data.result;
 };
